97bbd9ae985d89869be089c749216c22d7b98700
Onur Cayci <onur.cayci@outlook.com>
2021-05-30 18:30:48 -0400

    Update Help command output for version 1 of the goshell

--- internal/command/command.go
+++ internal/command/command.go
@@ -8,8 +8,13 @@ import (
 func Help() {
 	helpText := `Go Shell v0.0.1
 Available Commands:
-help		Prints out help text
-exit		Exits the shell
+COMMAND				DESCRIPTION
+
+help				Displays all the commands
+quit				Exits / terminates the shell with "Bye!"
+set VAR STRING		Assigns a value to shell memory
+print VAR			Displays the STRING assigned to VAR
+run SCRIPT.TXT		Executes the file SCRIPT.TXT
 `
 	fmt.Println(helpText)
 }
