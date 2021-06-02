f3040476a311fc8917e4f3d89c8bfa31be1d5bb3
Onur Cayci <onur.cayci@outlook.com>
2021-06-01 20:52:57 -0400

    Rename field IP of PCB to PC

--- internal/pcb/pcb.go
+++ internal/pcb/pcb.go
@@ -1,13 +1,13 @@
 package pcb
 
 type PCB struct {
-	IP    int
+	PC    int
 	Start int
 	End   int
 	Next  *PCB
 }
 
 func New(start, end int) *PCB {
-	newPCB := PCB{IP: start, Start: start, End: end}
+	newPCB := PCB{PC: start, Start: start, End: end}
 	return &newPCB
 }
