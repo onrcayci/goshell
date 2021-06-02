69af77f08e7508c936175695b7ae7f75df7bd335
Onur Cayci <onur.cayci@outlook.com>
2021-06-01 20:58:55 -0400

    Implement cpu package

--- internal/cpu/cpu.go
+++ internal/cpu/cpu.go
@@ -1,1 +1,27 @@
 package cpu
+
+import (
+	"github.com/onrcayci/goshell/internal/interpreter"
+	"github.com/onrcayci/goshell/internal/parser"
+	"github.com/onrcayci/goshell/internal/ram"
+)
+
+type CPU struct {
+	IP     int
+	IR     string
+	Quanta int
+}
+
+func New() *CPU {
+	newCPU := CPU{}
+	return &newCPU
+}
+
+func (c *CPU) Run() {
+	for c.Quanta != 0 {
+		c.IR = ram.RAM[c.IP]
+		c.IP++
+		argc, argv := parser.ParseInput(c.IR)
+		interpreter.Interpreter(argc, argv)
+	}
+}
