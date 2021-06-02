592a064f1cdfb6404ac7145c0ae5d0e22ecf14e9
Onur Cayci <onur.cayci@outlook.com>
2021-06-01 20:51:49 -0400

    Implement pcb package

--- internal/pcb/pcb.go
+++ internal/pcb/pcb.go
@@ -1,1 +1,13 @@
 package pcb
+
+type PCB struct {
+	IP    int
+	Start int
+	End   int
+	Next  *PCB
+}
+
+func New(start, end int) *PCB {
+	newPCB := PCB{IP: start, Start: start, End: end}
+	return &newPCB
+}
